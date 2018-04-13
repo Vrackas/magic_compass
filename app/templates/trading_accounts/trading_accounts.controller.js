b8cb7d1be843353ba97c70706da7fe3a42cd8e0f
Vitalii <vista@vistaMacBookPro.local>
2018-04-14 00:59:40 +0300

    Added content to trading accounts page

--- app/templates/trading_accounts/trading_accounts.controller.js
+++ app/templates/trading_accounts/trading_accounts.controller.js
@@ -10,6 +10,24 @@
     function HomepageController() {
         let vm = this;
 
+        vm.selected = [];
+
+        vm.query = {
+            order: 'name',
+            limit: 5,
+            page: 1
+        };
+
+        vm.desserts=[
+
+        ]
+        function success(desserts) {
+            vm.desserts = desserts;
+        }
+
+        vm.getDesserts = function () {
+            // vm.promise = $nutrition.desserts.get($scope.query, success).$promise;
+        };
 
     }
 })();
