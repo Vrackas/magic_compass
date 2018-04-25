7686c834b80eb58a0ec967008ab5198fc0d3955d
kravcovvalentin@gmail.com <kravcovvalentin@gmail.com>
2018-04-26 01:41:23 +0300

    add click on menu header_lang

--- app/directives/header_language_block/header_language_block.directive.controller.js
+++ app/directives/header_language_block/header_language_block.directive.controller.js
@@ -11,5 +11,19 @@
     function headerLanguageBlockController($state) {
         let vm = this;
 
+
+        vm.openLang = openLang;
+
+        function openLang(style) {
+            console.log($(style));
+            $(style).addClass('active');
+
+            $(document).mouseup(function (e) {
+                let container = $(style);
+                if (container.has(e.target).length === 0){
+                    container.removeClass('active');
+                }
+            });
+        }
     }
 })();
