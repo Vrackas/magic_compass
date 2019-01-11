d3d44b69564e167f5ea2e2f5d69c21d799b24401
Valentine <kravcovvalentin@gmail.com>
2019-01-11 15:34:06 +0200

    add features

--- app/templates/career/career.controller.js
+++ app/templates/career/career.controller.js
@@ -11,7 +11,26 @@
         let vm = this;
 
 
+        $(document).ready(function () {
 
+            // accordion effect
+
+            let acc = document.getElementsByClassName("accordion");
+            let i;
+
+            for (i = 0; i < acc.length; i++) {
+                acc[i].addEventListener("click", function () {
+                    this.classList.toggle("active");
+                    let panel = this.nextElementSibling;
+                    if (panel.style.maxHeight) {
+                        panel.style.maxHeight = null;
+                    } else {
+                        panel.style.maxHeight = panel.scrollHeight + "px";
+                    }
+                });
+            }
+
+        });
 
     }
 })();
